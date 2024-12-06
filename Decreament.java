969c100b6d703a0543a1265b3a39f012a8948c5c
Hindu <rajuhindur@gmail.com>
2024-12-06 09:35:50 +0530

    Add files via upload

---
+++ Decreament.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Decreament
+{
+    public static void main(String[] args)
+    {
+     int a=5;
+     System.out.println("pre="+(--a));
+     System.out.println("post="+(a--));
+    }
+}
---
+++ increament.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Increament
+{
+    public static void main(String[] args)
+    {
+     int a=7;
+     System.out.println("pre="+(++a));
+     System.out.println("post="+(a++));
+    }
+}
